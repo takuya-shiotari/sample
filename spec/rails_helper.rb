21ed87d36066f22a1c258b392355d9551a2b9fec
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-05-09 14:08:48 +0900

    Fix rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
-require 'simplecov'
-SimpleCov.start
+unless ARGV.include?('--only-failures')
+  require 'simplecov'
+  SimpleCov.start
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
