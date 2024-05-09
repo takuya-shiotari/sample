6457635ebaf99a9aeb5e51f6a495293e14e17d72
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-05-09 18:04:55 +0900

    Add spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,15 +1,22 @@
 RSpec.describe Address do
-  describe '#name' do
-    it 'returns name' do
-      address = Address.new(pref: 'tokyo')
+  describe '#pref' do
+    it 'returns pref' do
+      address = Address.new(pref: 'tokyo', city: 'setagaya')
       expect(address.pref).to eq 'tokyo'
+    end
+  end
+
+  describe '#city' do
+    it 'returns city' do
+      address = Address.new(pref: 'tokyo', city: 'setagaya')
+      expect(address.city).to eq 'setagaya'
     end
   end
 
   describe '#to_s' do
     it 'returns name value' do
-      address = Address.new(pref: 'tokyo')
-      expect(address.to_s).to eq 'tokyo'
+      address = Address.new(pref: 'tokyo', city: 'setagaya')
+      expect(address.to_s).to eq 'tokyosetagaya'
     end
   end
 end
