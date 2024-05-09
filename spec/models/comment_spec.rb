03f6862e5b7400626338fc0a781a13d97ef3bd92
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-05-09 13:08:35 +0900

    Fix spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Comment do
   describe '#body' do
     it 'returns body value' do
       comment = Comment.new('body')
-      expect(comment.body).to be 'body'
+      expect(comment.body).to eq 'body'
     end
   end
 
   describe '#to_s' do
     it 'returns body value' do
       comment = Comment.new('body')
-      expect(comment.to_s).to be 'body'
+      expect(comment.to_s).to eq 'body'
     end
   end
 end
