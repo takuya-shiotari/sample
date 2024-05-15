5c0b922d48e8d8c48db51d4620678a5f07c31f05
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-05-15 13:41:31 +0900

    Fix datadog_tracer

--- config/initializers/datadog_tracer.rb
+++ config/initializers/datadog_tracer.rb
@@ -2,9 +2,16 @@ require 'datadog/statsd'
 require 'ddtrace'
 
 
+=begin
 Datadog::Tracing.before_flush do |trace|
+  trace.spans.each do |span|
+    if span.resource == 'SELECT "posts".* FROM "posts"'
+      span.resource = span.resource.gsub('posts', 'tests')
+    end
+  end
   trace
 end
+=end
 
 Datadog.configure do |c|
   c.tracing.enabled = true
@@ -15,4 +22,14 @@ Datadog.configure do |c|
   c.tracing.instrument :rack, quantize: { query: { show: :all } }
   c.profiling.enabled = true
   c.service = "sample-#{Rails.env}"
+
+=begin
+  c.tracing.sampler = Datadog::Tracing::Sampling::PrioritySampler.new(
+    post_sampler: Datadog::Tracing::Sampling::RuleSampler.new(
+      [
+        Datadog::Tracing::Sampling::SimpleRule.new(service: 'sample-development', sample_rate: 1.0000)
+      ]
+    )
+  )
+=end
 end
