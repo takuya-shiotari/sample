746a7b9b09c9a585d5f58754a3998eeeda58914a
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-06-18 17:11:22 +0900

    ESLint review test2

--- app/assets/javascripts/applicatoin.js
+++ app/assets/javascripts/applicatoin.js
@@ -1,9 +1,9 @@
-function foo () {
-  return 'foo';
+function foo() {
+  return "foo";
 }
 
 console.log(foo()); // eslint-disable-line no-console
 
 function bar() {
-  return "bar";
+  return 'bar';
 }
