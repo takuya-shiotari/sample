8db3fb644e00a85b038b423b8a38b3405f512930
Takuya Shiotari <takuya.shiotari@gmail.com>
2024-06-27 09:17:09 +0900

    Fix generate_rspec_reviewdog_json

--- scripts/generate_rspec_reviewdog_json.rb
+++ scripts/generate_rspec_reviewdog_json.rb
@@ -1,40 +1,46 @@
+require 'nokogiri'
+
 $LOAD_PATH.unshift 'spec'
 ENV['SKIP_DB_MIGRATION'] = '1'
 require 'rails_helper'
 
-Dir.glob('spec/**/*_spec.rb').each do |file_path|
-  require_relative "../#{file_path}"
+# @param example_group [RSpec::Core::ExampleGroup]
+# @return [Array<RSpec::Core::Example>]
+def traverse_example_group(example_group)
+  example_group.examples + example_group.children.map(&method(:traverse_example_group)).flatten
 end
 
 # @param example_group [RSpec::Core::ExampleGroup]
-# @param full_description_to_line_number [Hash{String => Integer}]
-def traverse_example_group(example_group, full_description_to_line_number)
-  example_group.examples.each do |example|
-    full_description_to_line_number[example.metadata[:full_description]] = example.metadata[:line_number]
-  end
-  example_group.children.each do |child_example_group|
-    traverse_example_group(child_example_group, full_description_to_line_number)
-  end
+# @return [Hash{String => Integer}]
+def generate_full_description_to_line_number(example_group)
+  traverse_example_group(example_group)
+    .each_with_object({}) { |example, obj| obj[example.metadata[:full_description]] = example.metadata[:line_number] }
 end
 
-full_description_to_line_number = {}
-RSpec::ExampleGroups.constants.each do |constant|
-  example_group = RSpec::ExampleGroups.const_get(constant)
-  traverse_example_group(example_group, full_description_to_line_number)
-end
+# @param path [String]
+# @return [Hash{String => Integer}]
+def full_description_to_line_number_generator
+  obj = {}
+  ->(path) {
+    return obj[path] if obj[path]
 
-require 'nokogiri'
+    example_group = eval(File.open(path).read)
+    obj[path] = generate_full_description_to_line_number(example_group)
+  }
+end
 
 # @param junit_xml_file_path [String]
-# @param full_description_to_line_number [Hash{String => Integer}]
 # @return [Array<Hash>]
-def generate_reviewdog_rows(junit_xml_file_path, full_description_to_line_number)
+def generate_reviewdog_rows(junit_xml_file_path)
+  generator = full_description_to_line_number_generator
   Nokogiri(File.open(junit_xml_file_path)).css('testsuite testcase failure').map do |failure_elem|
     elem = failure_elem.parent
+    path = elem.attr('file')
+    full_description_to_line_number = generator.call(path)
     {
       message: failure_elem.text,
       location: {
-        path: elem.attr('file'),
+        path: path,
         range: {
           start: {
             line: full_description_to_line_number[elem.attr('name')]
@@ -47,7 +53,7 @@ end
 
 File.open(ENV['OUTPUT_FILE_PATH'], 'w') do |f|
   Dir.glob(ENV['JUNIT_XML_FILE_PATH_PATTERN']).each do |junit_xml_file_path|
-    rows = generate_reviewdog_rows(junit_xml_file_path, full_description_to_line_number)
+    rows = generate_reviewdog_rows(junit_xml_file_path)
     f.puts(rows.map(&:to_json).join("\n")) if rows.present?
   end
 end
