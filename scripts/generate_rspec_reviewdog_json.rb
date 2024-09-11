93f3d515ac4261c8d31ab84da14f8c0595f51335
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-09-11 10:02:00 +0900

    Fix

--- scripts/generate_rspec_reviewdog_json.rb
+++ scripts/generate_rspec_reviewdog_json.rb
@@ -4,46 +4,50 @@ $LOAD_PATH.unshift 'spec'
 ENV['SKIP_DB_MIGRATION'] = '1'
 require 'rails_helper'
 
+# RSpecのExampleGroupを再帰的に辿り、全てのExampleを取得する
 # @param example_group [RSpec::Core::ExampleGroup]
 # @return [Array<RSpec::Core::Example>]
-def traverse_example_group(example_group)
-  example_group.examples + example_group.children.map(&method(:traverse_example_group)).flatten
+def collect_all_examples(example_group)
+  example_group.examples + example_group.children.flat_map(&method(:collect_all_examples))
 end
 
+# ExampleGroupからfull_descriptionとline_numberの対応関係を生成する
 # @param example_group [RSpec::Core::ExampleGroup]
 # @return [Hash{String => Integer}]
-def generate_full_description_to_line_number(example_group)
-  traverse_example_group(example_group)
+def map_description_to_line_number(example_group)
+  collect_all_examples(example_group)
     .each_with_object({}) { |example, obj| obj[example.metadata[:full_description]] = example.metadata[:line_number] }
 end
 
+# キャッシュを利用して、ファイルパスに対応するfull_descriptionとline_numberの対応関係を生成する
 # @param path [String]
-# @return [Hash{String => Integer}]
-def full_description_to_line_number_generator
-  obj = {}
-  ->(path) {
-    return obj[path] if obj[path]
-
-    example_group = eval(File.open(path).read)
-    obj[path] = generate_full_description_to_line_number(example_group)
-  }
+# @return [Proc]
+def cached_description_to_line_number
+  cache = {}
+  lambda do |path|
+    cache[path] ||= begin
+      example_group = eval(File.read(path)) # rubocop:disable Security/Eval
+      map_description_to_line_number(example_group)
+    end
+  end
 end
 
+# JUnit XMLファイルからReviewdog形式のデータを生成する
 # @param junit_xml_file_path [String]
 # @return [Array<Hash>]
-def generate_reviewdog_rows(junit_xml_file_path)
-  generator = full_description_to_line_number_generator
+def parse_junit_failures(junit_xml_file_path)
+  description_mapper = cached_description_to_line_number
   Nokogiri(File.open(junit_xml_file_path)).css('testsuite testcase failure').map do |failure_elem|
     elem = failure_elem.parent
     path = elem.attr('file')
-    full_description_to_line_number = generator.call(path)
+    description_to_line = description_mapper.call(path)
     {
       message: failure_elem.text,
       location: {
         path: path,
         range: {
           start: {
-            line: full_description_to_line_number[elem.attr('name')]
+            line: description_to_line[elem.attr('name')]
           }
         }
       }
@@ -52,8 +56,8 @@ def generate_reviewdog_rows(junit_xml_file_path)
 end
 
 File.open(ENV['REVIEWDOG_JSON_FILE_PATH'], 'w') do |f|
-  Dir.glob(ENV['JUNIT_XML_FILE_PATH_PATTERN']).each do |junit_xml_file_path|
-    rows = generate_reviewdog_rows(junit_xml_file_path)
+  Dir[ENV['JUNIT_XML_FILE_PATH_PATTERN']].each do |junit_xml_file_path|
+    rows = parse_junit_failures(junit_xml_file_path)
     f.puts(rows.map(&:to_json).join("\n")) if rows.present?
   end
 end
