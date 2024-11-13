701901794feae8b4e6dccdcb5cb8df62a024362a
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-11-13 19:33:34 +0900

    Add flaky test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Post do
     it 'validates models' do
       post = Post.new(title: 'title', body: 'body')
       post.save
-      expect(post.valid?).to be true
+      expect(post.valid?).to be [true, false, false, false].sample
     end
   end
 
