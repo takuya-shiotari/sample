21f79948a44211478732a05a52096f972d62a760
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-11-15 15:09:26 +0900

    Fix

--- spec/models/post10_spec.rb
+++ spec/models/post10_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Post10' do
     it 'validates models' do
       post = Post.new(title: 'title', body: 'body')
       post.save
-      expect(post.valid?).to be [true, false, true, false].sample
+      expect(post.valid?).to be true
     end
   end
 
