2bd306493549b3120685269e853528ac95f13f2b
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-11-20 17:38:48 +0900

    Add Comment

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe Comment do
+  describe '#body' do
+    it 'returns body value' do
+      comment = Comment.new('body')
+      expect(comment.body).to eq 'body'
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns body value' do
+      comment = Comment.new('body')
+      sleep(1)
+      expect(comment.to_s).to eq 'body'
+    end
+  end
+
+  describe '#==' do
+    it 'checks comment equality' do
+      comment = Comment.new('body')
+      sleep(2)
+      expect(comment).to eq Comment.new('body')
+    end
+  end
+end
